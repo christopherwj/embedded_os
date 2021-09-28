cfa8f78bccd8536ac230d2d126ce5e672d90030a
christopherwj <38334130+christopherwj@users.noreply.github.com>
2021-09-27 20:14:43 -0700

    Update proj_1_base_case.c
    Changed terribly formatting due to copy paste from another machine

--- proj_1_base_case.c
+++ proj_1_base_case.c
@@ -4,29 +4,33 @@
 #include <time.h>
 
 
+
 float sum(int sum_to_value){
 
-int i = 0;
-clock_t start, end;
-float cpu_time_used;
+    int i = 0;
+    clock_t start, end;
+    float cpu_time_used;
+    float sum = 0;
 
-int arr[sum_to_value];
+    int arr[sum_to_value];
 
-//start the clock to measuring time 
-start = clock();
+    //start the clock to measuring time 
+    start = clock();
 
-//baseline sum to 9999999
-for (int i = 0; i < sum_to_value; i++)
-    sum += arr[i];
-// while (i < sum_to_value-1){
-// 	i++;
-// }
+    //make it non-inclusive for the last element
+    sum_to_value = sum_to_value -1;
 
-//stop the clock measuring time
-end = clock();
+    //baseline sum to 9999999
+    for (int i = 0; i < sum_to_value; i++)
+        sum += sum + i;
+    }
 
-cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-return cpu_time_used;
+    //stop the clock measuring time
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    return cpu_time_used;
 }
 
 
@@ -41,6 +45,7 @@ printf("Test 1x10^9 took %f\n", test2);
 float test3 = sum(10000000000);
 printf("Test 1x10^10 took %f\n", test3);
 
+printf("The total sum is:%f", test3);
 return 0;
 
 }
